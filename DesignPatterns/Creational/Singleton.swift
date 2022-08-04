4e889529da6f5a4f1e3567fda610571715dd7bde
Valentina Evdokimova <evdokimovalentina00@gmail.com>
2022-08-04 08:55:29 +0300

    Add Singleton

---
+++ DesignPatterns/Creational/Singleton.swift
@@ -0,0 +1,17 @@
+//
+//  L.swift
+//  DesignPatterns
+//
+//  Created by Валентина Евдокимова on 04.08.2022.
+//
+
+import Foundation
+
+final class Data {
+    static let shared = Data()
+    
+    private init() {
+        
+    }
+}
+
---
+++ DesignPatterns/Usage/Usage.swift
@@ -0,0 +1,15 @@
+//
+//  Usage.swift
+//  DesignPatterns
+//
+//  Created by Валентина Евдокимова on 04.08.2022.
+//
+
+import Foundation
+
+// MARK: Singleton
+let data = Data.shared
+
+
+
+
