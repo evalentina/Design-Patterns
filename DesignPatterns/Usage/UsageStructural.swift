a7b6fea434927aba894477d58e4d1eec9fde2306
Valentina Evdokimova <evdokimovalentina00@gmail.com>
2022-08-08 11:19:07 +0300

    Add usage decorator

---
+++ DesignPatterns/Usage/UsageStructural.swift
@@ -0,0 +1,14 @@
+//
+//  UsageStructural.swift
+//  DesignPatterns
+//
+//  Created by Валентина Евдокимова on 08.08.2022.
+//
+
+import Foundation
+
+// MARK: Decorator
+let espresso = Espresso()
+let capuccino = Milk(сoffee: espresso)
+let capuccinoWithSyrup = Syrup(сoffee: capuccino)
+
