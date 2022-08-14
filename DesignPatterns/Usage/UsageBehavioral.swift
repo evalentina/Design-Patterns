446ec3d6865ea904958d22ec32209e970f8ca493
Valentina Evdokimova <evdokimovalentina00@gmail.com>
2022-08-14 09:20:46 +0300

    Add command usage

---
+++ DesignPatterns/Usage/UsageBehavioral.swift
@@ -0,0 +1,15 @@
+//
+//  UsageBehavioral.swift
+//  DesignPatterns
+//
+//  Created by Валентина Евдокимова on 14.08.2022.
+//
+
+import Foundation
+
+// MARK: Command
+let door = Door(doorName: "FirstDoor")
+//door.open()
+//door.close()
+
+
